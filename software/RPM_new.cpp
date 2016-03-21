199c3c350bb7741fb36fb76b4ff93f0e81fc61da
ycgasjy <ycgasjy@gmail.com>
2016-03-20 20:43:13 -0400

    update

---
+++ software/RPM_new.cpp
@@ -0,0 +1,27 @@
+//Purpose: A better Recursive Partioning Multicast algorithm than Wang_NoC09
+//Author: Jiayi Sheng
+//Organization: CAAD lab @ Boston University
+//Start date: Mar 28th 2015
+//Input:  a file describes the src and destination lists
+//output: reduction_tree.txt that feeds to the table_gen.cpp
+
+#define X 4
+#define Y 4
+#define Z 4
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void read_src_dst_file(string filename){
+	ifstream input_file;
+	input_file.open(filename);
+	int line_counter = 0;
+	for ()
+
+}
+
+int main(){
+	string filename = "C:/Users/Jiayi/Documents/GitHub/MD_reduction/software/destination"
+	return 0;
+}
