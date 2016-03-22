9b4201e02d3126394acdfd4e6dcc51bed4d66cd8
ycgasjy <ycgasjy@gmail.com>
2016-03-22 18:18:45 -0400

    update

--- software/RPM_new.cpp
+++ software/RPM_new.cpp
@@ -13,12 +13,85 @@
 #include<string>
 using namespace std;
 
+struct src_dst_list{
+	int x;
+	int y;
+	int z;
+	bool src_or_dst;//true is src, false is dst
+	src_dst_list* next;
+};
+
+struct src_dst_list** src_list; 
+
+struct chunk{
+	//data structure to express a divided chunk
+	int x_downlim;
+	int x_uplim;
+	int y_downlim;
+	int y_uplim;
+	int z_downlim;
+	int z_uplim;
+	int get_x_size(){
+		if (x_downlim >= x_uplim){
+			return x_downlim-x_uplim+1;
+		}
+		else if (x_uplim>x_downlim){
+			return X - x_uplim + x_downlim;
+		}
+	}
+	int get_y_size(){
+		if (y_downlim >= y_uplim){
+			return y_downlim - y_uplim+1;
+		}
+		else if (y_uplim>y_downlim){
+			return Y - y_uplim + y_downlim;
+		}
+	}
+	int get_z_size(){
+		if (z_downlim >= z_uplim){
+			return z_downlim - z_uplim+1;
+		}
+		else if (z_uplim>z_downlim){
+			return Z - z_uplim + z_downlim;
+		}
+	}
+	bool within_x(int x){
+		if (x_uplim > x_downlim){
+			return x >= x_downlim && x <= x_uplim;
+		}
+		else{
+			return x <= x_downlim && x >= x_uplim;
+		}
+	}
+
+	bool within_y(int y){
+		if (y_uplim > y_downlim){
+			return y >= y_downlim && y <= y_uplim;
+		}
+		else{
+			return y <= y_downlim && y >= y_uplim;
+		}
+	}
+	bool within_z(int z){
+		if (z_uplim > z_downlim){
+			return z >= z_downlim && z <= z_uplim;
+		}
+		else{
+			return z <= z_downlim && z >= z_uplim;
+		}
+	}
+
+	
+	
+
+};
+
 void read_src_dst_file(string filename){
 	ifstream input_file;
 	input_file.open(filename);
 	int line_counter = 0;
 	while (!input_file.eof){
-
+		
 	}
 		
 
@@ -27,6 +100,6 @@ void read_src_dst_file(string filename){
 }
 
 int main(){
-	string filename = "C:/Users/Jiayi/Documents/GitHub/MD_reduction/software/destination.txt"
+	string filename = "C:/Users/Jiayi/Documents/GitHub/MD_reduction/software/destination.txt";
 	return 0;
 }
